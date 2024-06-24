8f36ae775ab2602eb02e4015e4789c85e011dc8c
lailaescoto <laila.escoto1@gmail.com>
2024-06-24 16:28:33 -0600

    pt 2 intro message
    i only did the intro, i wanted you guys to check pt1 first before copying it over to be modified

---
+++ pt2phoneProfitCalculator.py
@@ -0,0 +1,11 @@
+#print intro message
+print("Welcome to Circle Phones’ Profit calculator.")
+print("")
+timePeriodSelection = input("""You can calculate the profit of the company according to a specific day or by a week or divide the week into weekdays and weekend
+
+Enter:
+1 - For specific Day
+2 - For the Week
+3 - For Week Business Days
+4 - For Weekend days
+0 - Exit \n""")
